57549b42f6ce527d42c2f20d969059137cef2005
brahmagithubrit <brahmah7011@gmail.com>
2025-01-19 11:55:22 +0530

    nine

---
+++ my_app/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "Red",
+          height: 200,
+          width: 200,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {" "}
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
